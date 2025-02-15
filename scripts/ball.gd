81b8f4a8c727e0bfc5d638ececa13b26659dddc5
warps0 <matias.trossero.1@gmail.com>
2025-02-15 13:35:47 -0300

    First level created! Also: 	- Tile sheet was changed 	- Power charge was added 	- Ball now bounce walls

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,26 +1,41 @@
 extends CharacterBody2D
 
+signal ball_stopped
+
 @onready var sprite: AnimatedSprite2D = $Sprite
 @onready var hitbox: CollisionShape2D = $Hitbox
 
 @onready var marker: Sprite2D = $Marker
+@onready var power_bar: ProgressBar = $PowerBar
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const MAX_SPEED = 500.0
+const MIN_SPEED = 100.0
 const FRICTION = 300
 
 
-func _unhandled_input(event: InputEvent) -> void:
-	if Input.is_action_pressed("ui_accept"):
-		velocity = (marker.position - sprite.position).normalized() * 500
-
-
 func _process(_delta: float) -> void:
+	if Input.is_action_pressed("ui_accept") and velocity == Vector2(0, 0):
+		power_bar.value -= power_bar.step
+		if power_bar.value <= 0:
+			power_bar.value = 100
+	if Input.is_action_just_released("ui_accept") and velocity == Vector2(0, 0):
+		var shoot_speed = (100.0 - power_bar.value) * 5.0
+		
+		if shoot_speed < 100.0:
+			shoot_speed = 100.0
+		
+		velocity = (marker.position - sprite.position).normalized() * shoot_speed
+		power_bar.value = 100
+		print(shoot_speed)
+	
 	if velocity == Vector2(0, 0):
 		sprite.animation = "still"
 		marker.show()
+		power_bar.show()
+		emit_signal("ball_stopped")
 	else:
 		sprite.animation = "moving"
+		power_bar.hide()
 		marker.hide()
 	
 	if marker.visible:
@@ -38,6 +53,9 @@ func _process(_delta: float) -> void:
 		marker.global_rotation_degrees = rad_to_deg(mouse_dir.angle()) + 90
 
 func _physics_process(delta: float) -> void:
-	if velocity != Vector2(0, 0):
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-		move_and_slide()
+	var collision = move_and_collide(velocity * delta)
+	
+	if collision:
+		velocity = velocity.bounce(collision.get_normal())
+	
+	velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
